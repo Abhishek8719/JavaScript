1019645145cf519823e449089c9eb2b4c2e40e32
Abhishek Choudhary <cabhishekchoudhary31@gmail.com>
2025-07-13 04:27:50 +0000

    precedence operator

--- js/js01.js
+++ js/js01.js
@@ -1,1 +1,12 @@
-console.log("Hello, World!");
+let sccore ="abhishek choudhary"
+
+let num = Number(sccore)
+
+console.log(typeof num);
+
+let nu =33;
+
+let string = String(nu);
+
+console.log(typeof string);
+
---
+++ js/js03.js
@@ -0,0 +1,18 @@
+
+
+console.log(3+4);
+
+console.log("3"+"4")
+
+console.log(typeof("4"+7))
+
+console.log(3+4+"7")
+
+console.log("3"+"4"+9)
+
+let x=0;
+x++
+console.log(x)
+
+++x
+console.log(x)     
