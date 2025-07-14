d7bf3e6c5ea55f64f4c602aee9cfd4d461aa0fd9
Abhishek Choudhary <cabhishekchoudhary31@gmail.com>
2025-07-14 18:35:07 +0000

    Number and Math

---
+++ js/js04.js
@@ -0,0 +1,20 @@
+const no = 23.3467
+
+no.toFixed(2);  
+console.log(no.toFixed(3)); 
+console.log(no.toString());
+
+console.log(no.toExponential(2)); 
+
+console.log(no.toPrecision(4));
+
+Math.round(no);
+console.log(Math.ceil(no));
+
+const numeber = 23.3467;
+console.log(Math.floor(numeber));
+console.log(Math.random());  
+console.log(Math.max(3, 4, 5, 6, 7, 8, 9, 10));
+console.log(Math.min(3, 4, 5, 6, 7, 8, 9, 10));
+
+console.log(Math.sqrt(16));
