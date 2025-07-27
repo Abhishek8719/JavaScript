2d6ca471c662a5e0ac9f82423a4982b413e4e599
Abhishek Choudhary <cabhishekchoudhary31@gmail.com>
2025-07-27 05:55:32 +0000

    Scope in javascript

---
+++ js/js08.js
@@ -0,0 +1,20 @@
+// const a = 23;
+// a=1;
+
+// if(true)
+// {
+//     let a = 34;
+//     console.log(a)
+// }
+// console.log(a)
+
+let a = 9
+function check (){
+
+    var b = 3
+    console.log(b)
+}
+
+check();
+console.log(a)
+console.log(b)
